6168e8e5f906a977363fcb77eee9bad307dedc5a
21951a0558 <109816384+21951a0558@users.noreply.github.com>
2023-12-20 21:39:01 +0530

    Update registration.js

--- http-server/registration.js
+++ http-server/registration.js
@@ -1,29 +1,34 @@
-let registrationForm = document.getElementById("registration-form");
-const dateOfBirthInput = document.getElementById('dob');
+let form = document.getElementById("login-form");
+const dobInput = document.getElementById('dob');
 
-dateOfBirthInput.addEventListener('input', (event) => {
+dobInput.addEventListener('input', (event) => {
     const dob = new Date(event.target.value);
     const now = new Date();
     const age = now.getFullYear() - dob.getFullYear();
 
-    if (age < 18 || age > 55) {
-        dateOfBirthInput.setCustomValidity('Please enter a valid date of birth between ages 18 and 55.');
-    } else {
-        dateOfBirthInput.setCustomValidity('');
+    if ((age < 18) || age > 55) {
+        dobInput.setCustomValidity('Please enter a valid date of birth between ages 18 and 55.');
+    }
+    else{
+        dobInput.setCustomValidity('');
     }
 });
 
-const getUserEntries = () => {
+const getentries = ()=>{
     let entries = localStorage.getItem("user-entries");
-    entries = entries ? JSON.parse(entries) : [];
+    if(entries){
+        entries = JSON.parse(entries);
+    }
+    else{
+        entries = [];
+    } 
     return entries;
 }
+let data = getentries();
 
-let userData = getUserEntries();
-
-const showUserEntries = () => {
-    const entries = getUserEntries();
-    const tableEntries = entries.map((entry) => {
+const showentries =()=>{
+    const entries = getentries();
+    const tabentries = entries.map((entry)=>{
         const nameCell = `<td>${entry.nm}</td>`;
         const emailCell = `<td>${entry.eml}</td>`;
         const passwordCell = `<td>${entry.pas}</td>`;
@@ -33,40 +38,39 @@ const showUserEntries = () => {
         return row;
     }).join("\n");
 
-    const table =
-        `<table class="table-auto w-full">
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Date of Birth</th>
-                <th>Accepted Terms?</th>
-            </tr>${tableEntries}
-        </table>`;
+    const table = 
+    `<table class="table-auto w-full">
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>dob</th>
+            <th>accepted terms?</th>
+        </tr>${tabentries}
+    </table>`;
 
-    let userEntriesTable = document.getElementById("user-entries");
-    userEntriesTable.innerHTML = table;
+    let details = document.getElementById("user-entries");
+    details.innerHTML = table;
 }
-
-const saveFormData = (event) => {
+const saveform = (event)=>{
     event.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const dateOfBirth = document.getElementById("dob").value;
-    const acceptTerms = document.getElementById("acceptTerms").checked;
+    const nm = document.getElementById("name").value; 
+    const eml = document.getElementById("email").value;
+    const pas = document.getElementById("password").value;
+    const db = document.getElementById("dob").value;
+    const ch = document.getElementById("acceptTerms").checked;
     const entry = {
-        nm: name,
-        eml: email,
-        pas: password,
-        db: dateOfBirth,
-        ch: acceptTerms
-    };
-    userData.push(entry);
-    localStorage.setItem("user-entries", JSON.stringify(userData));
-    showUserEntries();
+        nm,
+        eml,
+        pas,
+        db,
+        ch
+    }
+    data.push(entry);
+    localStorage.setItem("user-entries",JSON.stringify(data));
+    showentries();
 }
 
-registrationForm.addEventListener("submit", saveFormData);
+form.addEventListener("submit",saveform); 
 
-showUserEntries();
+showentries();
